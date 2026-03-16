ac10094272232f764e82dca8c8d31267c8910a05
Lester James Miranda <ljvmiranda@gmail.com>
2026-03-16 09:21:28 +0000

    [wip] Update

--- analysis/infra_lingdiv.py
+++ analysis/infra_lingdiv.py
@@ -16,6 +16,10 @@ def main():
         "https://ourworldindata.org/grapher/world-bank-income-groups.csv?v=1&csvType=full&useColumnShortNames=true",
         storage_options={"User-Agent": "Our World In Data data fetch/1.0"},
     )
+    ict_adoption_df = pd.read_csv(
+        "https://ourworldindata.org/grapher/ict-adoption-per-100-people.csv?v=1&csvType=full&useColumnShortNames=true",
+        storage_options={"User-Agent": "Our World In Data data fetch/1.0"},
+    )
     network_df = pd.read_csv(
         "https://ourworldindata.org/grapher/population-covered-by-mobile-network-by-network-capability.csv?v=1&csvType=full&useColumnShortNames=true",
         storage_options={"User-Agent": "Our World In Data data fetch/1.0"},
